6a2535b681005615c00009f4a3061ead2b9ceb33
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-30 18:58:09 -0300

    feat: init basic navigation

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {SignIn, CreateAccount} from './Screens'
 
-export default function App() {
+const AuthStack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="SignIn" component={SignIn} options={{title: 'Sign In'}} />
+      <AuthStack.Screen name="CreateAccount" component={CreateAccount} options={{title: 'Create Account'}} />
+    </AuthStack.Navigator>
+  </NavigationContainer>
+  )
 }
+  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
